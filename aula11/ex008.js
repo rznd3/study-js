59b88960beba6055016708ffe293389879d1d978
rznd <grezende310@gmail.com>
2023-08-04 22:56:50 -0300

    conditions

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 10
+
+console.log(`The speed of your car is ${vel} km/h`)
+    if (vel > 60){
+        console.log('You exceeded the speed limit. Fined.')
+    }
+
+console.log('Always drive using a seatbelt.')
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var country = 'Brasil'
+
+console.log(`Living in ${country}`)
+
+if (country == 'Brasil'){
+    console.log('You are Brazilian')
+} else {
+    console.log('You are a foreigner')
+}
